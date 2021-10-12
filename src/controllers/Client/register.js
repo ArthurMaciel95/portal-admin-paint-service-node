898fb05e50327c2f4d8316dfd71dcdc41aafa1ca
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-12 20:16:26 -0300

    :art: adicionando validaçao email ja existente

--- src/controllers/Client/register.js
+++ src/controllers/Client/register.js
@@ -2,6 +2,15 @@ const User = require('../../models/Client/client');
 
 module.exports = async (req, res) => {
   try {
+    const emailAlreadyExist = await User.findOne({ email: req.body.email });
+
+    if (emailAlreadyExist) {
+      res.json({
+        status: false,
+        message: 'Email já existe.',
+      });
+    }
+
     const user = await User.create(req.body);
     res.json({
       data: user,
