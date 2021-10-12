648d52f2417775e03416f70082eb330685a64841
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-12 20:26:23 -0300

    :art: criando endpoint para criar cliente

--- src/controllers/Client/register.js
+++ src/controllers/Client/register.js
@@ -1,10 +1,23 @@
-module.exports = (req, res) => {
-    try {
-        res.json({
-            status: true,
-            message: "Register",
-        });
-    } catch (erro) {
-        console.error(erro);
+const User = require('../../models/Client/client');
+
+module.exports = async (req, res) => {
+  try {
+    const emailAlreadyExist = await User.findOne({ email: req.body.email });
+
+    if (emailAlreadyExist) {
+      res.json({
+        status: false,
+        message: 'Email já existe.',
+      });
     }
+
+    const user = await User.create(req.body);
+    res.json({
+      data: user,
+      status: true,
+      message: 'usuário criando com sucesso!',
+    });
+  } catch (erro) {
+    console.error(erro);
+  }
 };
