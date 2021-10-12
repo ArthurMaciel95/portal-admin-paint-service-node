52bd9165df452281a9e87ff7a79a187290ba1406
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-12 18:33:50 -0300

    :art: criando models

---
+++ src/models/Client/client.js
@@ -0,0 +1,45 @@
+const { Schema }, mongoose = require('mongoose');
+
+const Client = new Schema(
+  {
+    uuid: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    cpf: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: 'pending',
+    },
+    payment_method: {
+      type: String,
+      required: true,
+    },
+    company: String,
+    photo: {
+      type: Buffer,
+    },
+    adress: {
+      cep: String,
+      district: String,
+      additional_infomation: String,
+      city: String,
+    },
+  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  },
+);
+
+module.exports = mongoose.model('users', Client);
