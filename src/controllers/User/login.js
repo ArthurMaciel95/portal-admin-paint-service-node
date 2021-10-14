9ad5a677f52625a616658d00205e1c055b24db7b
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-14 16:48:26 -0300

    :recycle: Adicionando comentario no codigo

--- src/controllers/User/login.js
+++ src/controllers/User/login.js
@@ -7,39 +7,67 @@ module.exports = async (req,res) => {
     try{
 
         toCompare.keys(['username','email','password'],req.body,true)
-
+        /**
+         * verifica se o password é uma string
+         * 
+         */
         if(!isNaN(req.body.password)){
             throw { message: 'password need to be string'}
         }
+
+        /**
+         * verifica se o password é um campo vazio
+         * 
+         */
         if(value.isNull(req.body.password)){
             throw { message : 'password cannot be null'}
         }
 
+        /**
+         * verifica se o email está vazio
+         * 
+         */
         if(value.isNull(req.body.email)){
             throw { message: 'email cannot be empty'}
-        }
+        }   
 
+        /**
+         * verifica se o email é valido
+         * 
+         */
         if(!email.isEmail(req.body.email)){
             throw { message: 'email is not valid'}
         }
 
+        /**
+         * procura no banco se tem alguem com esté email
+         * 
+         */
         const user = await User.findOne({email:req.body.email})
         console.log(user)
+
+        /**
+         * verifica se encontrou o usuário
+         * 
+         */
         if(!user){
             throw { message: 'user not found'}
         }
       
         const {password} = user
 
+        /**
+         * compara a senha enviada com a senha do banco 
+         * 
+         */
         const comparePassword = await bcryptjs.compare(req.body.password,password)
-        console.log(comparePassword)
+        
         if(!comparePassword)throw {message: 'email or password is not correct'}
          
         /* 
             GERAR JSON WEB TOKEN PARA A SESSÃO
         */
-        console.log('logado com succeso')
-     
+
         res.status(200).json({
             status:true,
             message:'logged with success'
